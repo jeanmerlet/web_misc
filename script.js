ad970c1169da160324eafdbf73c64d0af4350765
Jean Merlet <jean.joseph.merlet@gmail.com>
2015-12-01 13:47:22 -0500

    Added color option

--- script.js
+++ script.js
@@ -19,37 +19,79 @@ window.addEventListener("load", function(){
 	/*change square color function*/
 	function pixelate() {
 		container.addEventListener("mouseover", function(e){
-		e.target.classList.add("black"); 
+			if (!color) {
+				e.target.style.backgroundColor = "black";
+			} else {
+				var r = Math.floor(Math.random()*255);
+				var g = Math.floor(Math.random()*255);
+				var b = Math.floor(Math.random()*255);
+				e.target.style.backgroundColor = "rgb("+r+","+g+","+b+")";
+			};
 		});
 	};
 
 	/*page initialization*/
-	
+
+	var border = document.getElementById("border");
 	var newDiv = document.createElement("div");
 	newDiv.id = "container";
-	document.body.appendChild(newDiv);
+	border.appendChild(newDiv);
 	var container = document.getElementById("container");
 	container.style.width = "600px";
+	var button = document.getElementsByTagName("button");
+	var color = 0;
+	var originalTitleText = document.getElementsByTagName("h1")[0].innerHTML;
 
 	makegrid(4);
 	pixelate();
 
-	/*clear + recreate button*/
-	var button = document.getElementsByTagName("button");
-	var boxes = document.querySelectorAll(".box");
+	/*clear button*/
 
 	button[0].addEventListener("click", function(){
+	var boxes = document.querySelectorAll(".box");
 
 		for (j = 0; j < boxes.length; j ++){
-		boxes[j].classList.remove("black");
+		boxes[j].style.backgroundColor="white";
+		};
+	});
+
+	/*resize button*/
+	button[1].addEventListener("click", function(){
+	var size = prompt("Squares per side?", "4");
+
+		while (container.firstChild) {
+			container.removeChild(container.firstChild);
 		};
 
-	var size = prompt("Squares per side?", "4");
-	while (container.firstChild) {
-		container.removeChild(container.firstChild);
-	};
 	makegrid(size);
-	pixelate();
+	});
+
+	/*color toggle button*/
+	button[2].addEventListener("click", function(){
+		var title = document.getElementsByTagName("h1")[0];
+		if (!color) {
+			color = 1;
+			var titleText = title.innerHTML;
+			title.innerHTML = '';
+			for (l = 0; l < titleText.length; l++){
+				var newSpan = document.createElement("span");
+				newSpan.innerHTML = titleText[l];
+				console.log(newSpan.innerHTML);
+				var r = Math.floor(Math.random()*255);
+				var g = Math.floor(Math.random()*255);
+				var b = Math.floor(Math.random()*255);
+				newSpan.style.color = "rgb("+r+","+g+","+b+")";
+				title.appendChild(newSpan);
+			}
+		} else {
+			color = 0;
+			while (title.firstChild) {
+				title.removeChild(title.firstChild);
+			};
+			title.innerHTML = originalTitleText;
+		};
 	});
 
 });
+
+
